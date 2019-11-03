a8c1713a418e01ce9316d2c02380192c85178c5b
Chris Heder <chris@chrisheder.com>
2019-11-03 01:18:25 -0400

    Added images uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  attr_accessible :name, :message
+  # attr_accessible :name, :message
+
+  has_many_attached :images
 
   validates :name,    :presence => true
   validates :message, :presence => true
